68574f67b2ce019edfa414f51031323d462644a8
mario <mariocm2002@hotmail.com>
2026-05-02 11:24:19 +0200

    better autocomplete

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,15 +3,27 @@ return {
   opts = function(_, opts)
     local cmp = require("cmp")
 
+    -- Keybinds
     opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
-
-      -- Disable Enter confirmation
       ["<CR>"] = cmp.config.disable,
-
-      -- Explicit confirm key
-      ["<C-y>"] = LazyVim.cmp.confirm({ select = true }),
-
-      -- Tab cycles forward
+      ["<M-y>"] = LazyVim.cmp.confirm({ select = true }),
+      -- ["<M-y>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-space>"] = cmp.mapping({
+        i = cmp.mapping.complete(),
+        c = function(_)
+          if cmp.visible() then
+            if not cmp.confirm({ select = true }) then
+              return
+            end
+          else
+            cmp.complete()
+          end
+        end,
+      }),
+      ["<C-q>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
@@ -19,8 +31,6 @@ return {
           fallback()
         end
       end, { "i", "s" }),
-
-      -- Shift-Tab cycles backward
       ["<S-Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_prev_item()
@@ -29,5 +39,29 @@ return {
         end
       end, { "i", "s" }),
     })
+
+    -- Sorting
+    opts.sorting = {
+      comparators = {
+        cmp.config.compare.offset,
+        cmp.config.compare.exact,
+        cmp.config.compare.score,
+        function(entry1, entry2)
+          local _, entry1_under = entry1.completion_item.label:find("^_+")
+          local _, entry2_under = entry2.completion_item.label:find("^_+")
+          entry1_under = entry1_under or 0
+          entry2_under = entry2_under or 0
+          if entry1_under > entry2_under then
+            return false
+          elseif entry1_under < entry2_under then
+            return true
+          end
+        end,
+        cmp.config.compare.kind,
+        cmp.config.compare.sort_text,
+        cmp.config.compare.length,
+        cmp.config.compare.order,
+      },
+    }
   end,
 }
